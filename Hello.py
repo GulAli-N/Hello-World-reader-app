eb7c62c81c92cda3f91471edba75370654555bfa
Gul1969 <71330969+Gul1969@users.noreply.github.com>
2021-01-13 14:22:13 +0000

    Create Hello.py

---
+++ Hello.py
@@ -0,0 +1,11 @@
+import logging
+import os
+import pprint
+
+logging.basicConfig(filename='gul.log', filemode='a', format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s', datefmt='%d-%m-%Y %H:%M:%S', level=logging.DEBUG)
+
+gul = os.environ['GUL']
+
+print("GUL:", gul)
+
+logging.info('Hello World'
