5d1afb4a5eacb635385a5900f5d8644eda8eba54
Gul1969 <71330969+Gul1969@users.noreply.github.com>
2021-01-19 13:46:59 +0000

    Add files via upload

---
+++ terra.tf
@@ -0,0 +1,18 @@
+resource "local_file" "Hello" {
+    content = <<EOT
+import logging
+import os
+
+
+# Logging information
+logging.basicConfig(filename='gul.log', format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s', datefmt='%d/%m/%Y %I:%M:%S %p', level=logging.INFO)
+
+# validation
+if "A_VARIABLE" in os.environ: 
+    print: "Validated"
+    logging.info("The environment variable has been imported")
+else: 
+    print: "Environment variable missing"
+EOT
+    filename = "/Users/Gul.Ali/Desktop/Hello.py"
+}
